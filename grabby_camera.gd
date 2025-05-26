7df6b16ca10dde23837b5512e3d4f44d39d99a49
jergling <the_eep@comcast.net>
2025-05-26 14:50:52 -0500

    we danglin
    Click-and-drag interaction works for a single object. Lift target point on box to the surface of collision layer 4 and holds it on an undamped spring.

--- grabby_camera.gd
+++ grabby_camera.gd
@@ -5,6 +5,9 @@ var targetling : Node3D
 var grabHandle : Node3D
 var dragging : bool = false
 var targetBody : RigidBody3D
+const CURSORLIFT : Vector3 = Vector3(0,3,0)
+const LIFTMASK = 0b00000000_00000000_00000000_00001000
+const DRAGGABLEMASK = 0b00000000_00000000_00000000_00000010
 
 func _input(event):
 	if event is InputEventMouse:
@@ -25,9 +28,15 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	var direction : Vector3 = targetling.global_position - targetBody.global_position
-	#var normalizedDirection : Vector3 = direction.normalized()
-	targetBody.apply_central_force(direction * 5.0)
+	if dragging:
+		var targetHandle = targetBody.get_child(2)
+		var separation : Vector3 = (targetling.global_position) - (targetHandle.global_position)
+		print("target global pos:" + str(targetling.global_position))
+		print("handle global pos:" + str(targetHandle.global_position))
+		print("target-handle separation:" + str(separation))
+		#var normalizedDirection : Vector3 = direction.normalized()
+		targetBody.apply_force(separation * 15.0, targetHandle.global_position - targetBody.global_position)
+		#targetBody.apply_central_force(separation * 15.0)
 	
 
 func updateMouseTarget(event) -> void:
@@ -36,16 +45,22 @@ func updateMouseTarget(event) -> void:
 	#print(viewMouseEventPos)
 	var unitRay : Vector3 =  project_ray_origin(viewMouseEventPos)
 	var rayLimit = project_position(viewMouseEventPos, 1000)
-	var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(unitRay, rayLimit))
-	#print(result)
-	if not result.is_empty():
-		targetling.position = result["position"]
-
+	if not dragging:
+		var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(unitRay, rayLimit, DRAGGABLEMASK))
+		#print(result)
+		if not result.is_empty():
+			targetling.position = result["position"]
+	else:
+		var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(unitRay, rayLimit, LIFTMASK))
+		#print(result)
+		if not result.is_empty():
+			targetling.position = result["position"]
+			
 func updateGrabHandle(event) -> void:
 	#we have freshly clicked
 	if not dragging and event.pressed:
 		grabHandle.global_position = targetling.global_position
-		targetBody.add_child(grabHandle)
+		targetBody.get_child(2).global_position = grabHandle.global_position
 		dragging = true
 	elif dragging and not event.pressed:
 		targetBody.remove_child(grabHandle)
