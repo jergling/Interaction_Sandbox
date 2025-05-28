4a469654f5dad0b6dd750bd9e3e10deca51a1f2f
jergling <the_eep@comcast.net>
2025-05-27 23:13:46 -0500

    first item
    created the "ray blaster", a test object. It is not behaving right and I need to work out blender's export properties to get a mesh and collider to export together.

--- tossman.gd
+++ tossman.gd
@@ -7,6 +7,9 @@ var baseMouseSens : float = 0.002
 var xMouseSens : float
 var yMouseSens : float
 
+var input_dir : Vector2
+var direction : Vector3
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const NINETYDEGREESINRADIANS = deg_to_rad(90.0)
@@ -26,14 +29,15 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("fp_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("fp_strafeleft", "fp_straferight", "fp_forward", "fp_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Control Input Chunk
+	if controlsAreLive:
+		if Input.is_action_just_pressed("fp_jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		input_dir = Input.get_vector("fp_strafeleft", "fp_straferight", "fp_forward", "fp_backward")
+		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
